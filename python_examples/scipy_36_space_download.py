4153b6bdebe8afd91cc795d49ee5d43bc317efb3
Jonghwa Park <suakii@gmail.com>
2015-10-28 10:42:34 +0900

    Update scipy_36_space_download.py
    파일 다운로드 경로 수정

--- python_examples/scipy_36_space_download.py
+++ python_examples/scipy_36_space_download.py
@@ -5,7 +5,7 @@ def retrieve_iss_images(begin, end):
     # begin and end are integers
     # Looping over image range and downloading
     for i in xrange(begin, end):
-        url = 'http://eol.jsc.nasa.gov/sseop/images/ESC/small/ISS030/ISS030-E-'
+        url = 'http://eol.jsc.nasa.gov/DatabaseImages/ESC/small/ISS030/ISS030-E-'
         url = url + str(i) + '.JPG'
         print('saving ' + str(i) + '.JPG ...')
         open(str(i) + '.JPG', 'wb').write(urllib2.urlopen(url).read())
